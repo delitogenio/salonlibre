cd657e089688f2546a2b69d7b4645a2a7c23db3c
cbetancur <cbetancur@emida.net>
2022-09-02 08:40:05 -0500

    DB Connection

--- Clases/asignacion.py
+++ Clases/asignacion.py
@@ -1,45 +1,69 @@
 import datetime
-from Metodos.DBConnection import Connection
-
+from Metodos.DBConnection import connection
 
 from Clases.materias import Materia
 from Clases.salon import Salon
 
+
 class Asignacion():
-    codigo= None
+    codigo = None
     materia = Materia()
     salon = Salon()
-    tiempoentrada=None
-    tiemposalida=None
-    conn = Connection().initiate_connection()
-    cur =conn.cursor()
-    def __init__(self, codigo,materia, salon,tiempoentrada,tiemposalida):
-        self.codigo=codigo
-        self.materia=materia
-        self.salon=salon
-        self.tiempoentrada= datetime.datetime.strptime(tiempoentrada,'%Y-%m-%d %H:%M:%S')
-        self.tiemposalida=datetime.datetime.strptime(tiemposalida,'%Y-%m-%d %H:%M:%S')
-    def set_asignacion(self,nombreMateria,profesor,edifcio,nombreespacio):
+    tiempoentrada = None
+    tiemposalida = None
+    cur = connection()
+
+    def __init__(self, codigo, materia, salon, tiempoentrada, tiemposalida):
+        self.codigo = codigo
+        self.materia = materia
+        self.salon = salon
+        self.tiempoentrada = datetime.datetime.strptime(tiempoentrada, '%Y-%m-%d %H:%M:%S')
+        self.tiemposalida = datetime.datetime.strptime(tiemposalida, '%Y-%m-%d %H:%M:%S')
+
+    def set_asignacion(self, nombreMateria, profesor, edifcio, nombreespacio):
         self.materia.set_nombreMateria(nombreMateria)
         self.materia.set_profesor(profesor)
         self.salon.set_edificio(edifcio)
         self.salon.set_nombre(nombreespacio)
+
     def get_codigo(self):
         return self.codigo
+
     def get_tiempoentrada(self):
         return self.tiempoentrada
+
     def get_horaentrada(self):
-        hora = datetime.datetime.strftime(self.tiempoentrada,"%X")
+        hora = datetime.datetime.strftime(self.tiempoentrada, "%X")
         return hora
+
     def get_horasalida(self):
-        hora = datetime.datetime.strftime(self.tiemposalida,"%X")
+        hora = datetime.datetime.strftime(self.tiemposalida, "%X")
         return hora
+
     def get_tiemposalida(self):
         return self.tiemposalida
-    def set_tiempoentrada(self,x):
-        self.tiempoentrada=x
-    def settiemposalida(self,x):
-        self.tiemposalida=x
+
+    def set_tiempoentrada(self, x):
+        self.tiempoentrada = x
+
+    def settiemposalida(self, x):
+        self.tiemposalida = x
+
+
+    def crearasignacion(self):
+        flagsalon = True
+        self.cur.execute("SELECT nombreEspacio,horaEntrada,horaSalida FROM Asignacion")
+        for (nombreEspacio,horaEntrada,horaSalida) in self.cur:
+            if nombreEspacio[0] == self.salon.get_nombre() or horaEntrada <= self.tiempoentrada <= horaSalida or horaEntrada <= self.tiemposalida<= horaSalida:
+                flagsalon=False
+        self.cur._clear_result()
+        if flagsalon:
+            self.cur.execute("INSERT INTO `ProgIDB`.`Asignacion` (`nombreMateria`, `nombreEspacio`, `profesor`, `edificio`, `horaEntrada`, `horaSalida`) VALUES (?, ?,?,?,?,?)",(
+                self.materia.get_nombreMateria(),self.materia.get_profesor(),self.salon.get_nombre(),
+                self.salon.get_edificio(),self.tiempoentrada,self.tiemposalida))
+        else: print("Asignacion fallida")
+
+
 
 
 
--- main.py
+++ main.py
@@ -7,23 +7,20 @@ from Clases.salon import Salon
 def run_test():
 
 
-    edifcio = '201'
-    nombreespacio = 'Harwdare Libre'
-    nombreMateria = 'POO'
-    tiempoentrada = '2022-08-26 18:00:00'
-    tiemposalida = '2022-08-26 20:00:00'
-    profesor = 'Cesar Torres'
+    edifcio = "A"
+    nombreespacio = "201"
+    nombreMateria = "Programacion"
+    tiempoentrada = '2022-09-01 15:30:00'
+    tiemposalida = '2022-09-01 18:30:01'
+    profesor = "Cesar Torres"
 
     materia = Materia()
     salon = Salon()
 
     asignacion = Asignacion(1541, materia, salon,tiempoentrada=tiempoentrada,tiemposalida=tiemposalida)
     asignacion.set_asignacion(nombreMateria, profesor, edifcio, nombreespacio)
-    print("El codigo de la asignacion es "+ str(asignacion.get_codigo()))
-    print("La hora de entrada es "+ asignacion.get_horaentrada())
-    print("La hora de salida es " + asignacion.get_horasalida())
-    print("El profesor asignado es " + asignacion.materia.get_profesor())
-    print("El salon es " + asignacion.salon.get_nombre() + " en el edificio "+ asignacion.salon.get_edificio() )
+    asignacion.crearasignacion()
+
 
 
 run_test()
