94a77d016a9d4c7672ed3990ee45513e928224a5
cbetancur <cbetancur@emida.net>
2022-09-07 14:09:35 -0500

    Flask Login Implementation

--- flaskr/Metodos/Login.py
+++ flaskr/Metodos/Login.py
@@ -24,7 +24,7 @@ class Login:
             self.contrasena = request.form["password"]
             for Usuario, Contrasena,Tipo_de_Usuario in cur:
                 if Usuario == self.nombreUsuario and Contrasena == self.contrasena:
-                    self.tipo_usuario=int.from_bytes(Tipo_de_Usuario,"little")
+                    self.tipo_usuario=Tipo_de_Usuario
                     if self.tipo_usuario == 1:
                         return 'admintemplate.html'
                     elif self.tipo_usuario == 2:
@@ -32,38 +32,44 @@ class Login:
                     elif self.tipo_usuario == 3:
                         return 'profesortemplate.html'
                     else: return '14'
-                else:
-                    return ('14')
         else:return 'login.html'
 
-    @bplogin.route('/login', methods=('GET', 'POST'))
+    @bplogin.route('/', methods=('GET', 'POST'))
     def __login():
+
         login = Login()
         result =login.login()
         if result != '14':
             return render_template(result)
         else:
             flash('Credenciales no validas','error')
-            return redirect ('/login/login')
-
-
-
-
-
-
-
+            return redirect ('/login')
     def create_login(self,tipo_usuario):
+        self.nombreUsuario = request.form["user"]
+        self.contrasena = request.form["password"]
+        self.tipo_usuario = int(tipo_usuario)
         conn = connection()
         cur = conn.cursor()
         flag =True
         cur.execute("SELECT Usuario,Contrasena FROM Usuarios")
         for Usuario, Contrasena in cur:
             if Usuario == self.nombreUsuario:
-                print("El usuario ya esta creado")
                 flag=False
+                return flash('El usuario ya esta creado','error')
         if flag:
-            cur.execute("INSERT INTO Usuarios(Usuario, Contrasena, Tipo_de_Usuario) VALUES (?,?,?)",(self.nombreUsuario, self.contrasena, tipo_usuario,))
+            cur.execute("INSERT INTO Usuarios(Usuario, Contrasena, Tipo_de_Usuario) VALUES (?,?,?)",(self.nombreUsuario, self.contrasena, self.tipo_usuario,))
             conn.commit()
+            flash('Usuario creado correctamente','message')
+
+
+    @bplogin.route('/createlogin', methods=('GET', 'POST'))
+    def __create_login ():
+        if request.method == 'GET':
+            return render_template('logincreation.html')
+        else:
+            login = Login()
+            login.create_login(request.form['lista_tipos_usuario'])
+            return redirect('/login')
 
 
 
